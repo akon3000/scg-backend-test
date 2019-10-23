5c88aaf3cc923247d118c93b1ed179e5810ad661
ToNoey <vonmane001@gmail.com>
2019-10-24 00:40:15 +0700

    Add .env file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const app = require('express')()
 const cache = require('./middleware/cache')
 const numberSeries = require('./utils/numberSeries')
 
+require('dotenv').config()
+
+
 app.get('/scg', cache(10), (req, res) => {
   return res.send('Hello SCG interview s')
 })
 
 app.get('/number-series', cache(10), (req, res) => {
   try {
-    const numbers = numberSeries([null, 5, 9, null, 23, null, null])
+    const numbers = numberSeries([null, 5, 9, 15, 23, null, null])
     return res.send(numbers)
   } catch (err) {
     return res.status(500).send(`Error: ${err.message}`)
