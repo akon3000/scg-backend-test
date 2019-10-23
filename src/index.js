a4adb5e27c27d0cb3074092cd9928bb390066ffc
ToNoey <vonmane001@gmail.com>
2019-10-24 01:12:39 +0700

    Add route search restaurants in Bangsue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const app = require('express')()
+const axios = require('axios')
 const cache = require('./middleware/cache')
 const numberSeries = require('./utils/numberSeries')
 
 require('dotenv').config()
 
+const { PORT, API_SEARCH, API_KEY } = process.env
 
 app.get('/scg', cache(10), (req, res) => {
   return res.send('Hello SCG interview s')
@@ -18,4 +20,14 @@ app.get('/number-series', cache(10), (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log(`Server runing on port 3000`))
+app.get('/scg-location', cache(10), async (req, res) => {
+  const { search = 'restaurants in Bangsue' } = req.query
+  try {
+    const response = await axios.get(encodeURI(`${API_SEARCH}?query=${search}&key=${API_KEY}`))
+    return res.send(response.data)
+  } catch (err) {
+    return res.status(500).send(`Error: some thing went worng`)
+  }
+})
+
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
