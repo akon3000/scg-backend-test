38aa1482c11df444e39c14b908c60f7b8dd1eb3f
ToNoey <vonmane001@gmail.com>
2019-10-24 13:16:39 +0700

    test deploy

--- line-message/index.js
+++ line-message/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send(`Hi there! This is a nodejs-line-message running on PORT: ${PORT}`)
+  res.send(`Hi there! This is a nodejs-line-message running on PORT: ${PORT} `)
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
